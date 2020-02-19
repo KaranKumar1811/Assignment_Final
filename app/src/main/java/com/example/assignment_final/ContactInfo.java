889c400f630cb233dc527ad42734faeca210f25b
Sandeep <Sandeepjangra0100@gmail.com>
2020-02-18 21:35:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/assignment_final/ContactInfo.java
@@ -0,0 +1,185 @@
+package com.example.assignment_final;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+public class ContactInfo extends AppCompatActivity {
+
+    SQLiteDatabase myDatabase ;
+    Cursor c ;
+    Adapter adapter =new Adapter(ContactInfo.this,myDatabase,c);
+    ListView contact_List;
+    SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contactinfo);
+        myDatabase = openOrCreateDatabase("person3", MODE_PRIVATE, null);
+        c = myDatabase.rawQuery("SELECT * FROM person3", null);
+        searchView = findViewById(R.id.searchView);
+        contact_List = findViewById(R.id.contact_list);
+        contact_List.setAdapter(adapter);
+
+
+
+
+        contact_List.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, final long id) {
+                Log.d("list ID : ",String.valueOf(id));
+                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(view.getContext());
+                alertDialogBuilder.setMessage("Are you sure, You wanted to make decision");
+                alertDialogBuilder.setPositiveButton("yes",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                myDatabase.delete("person3","id = "+ String.valueOf(id),null);
+                                adapter.notifyDataSetChanged();
+                                finish();
+                            }
+                        });
+                alertDialogBuilder.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+                return true;
+            }
+        });
+        adapter.notifyDataSetChanged();
+
+
+
+    }
+
+
+    public void loadData(){
+
+
+    }
+
+    class Adapter extends BaseAdapter
+    {
+        Activity activity;
+
+
+
+        SQLiteDatabase mDB;
+        Cursor cursor ;
+
+        public Adapter(Activity activity, SQLiteDatabase mDB, Cursor cursor) {
+            this.activity = activity;
+            this.mDB = mDB;
+            this.cursor = cursor;
+        }
+
+
+
+        @Override
+        public int getCount() {
+            return c.getCount();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return c.getPosition();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return c.getPosition();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ContactHolder holder=null;
+            if(convertView==null) {
+                holder = new ContactHolder();
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_layout, parent, false);
+
+                holder.firstname = convertView.findViewById(R.id.first_name);
+                holder.lastname = convertView.findViewById(R.id.last_name);
+                holder.phoneNo = convertView.findViewById(R.id.contact);
+                holder.address = convertView.findViewById(R.id.address);
+
+
+
+
+                convertView.setTag(holder);
+
+            }
+            else
+            {
+                holder = (ContactHolder) convertView.getTag();
+            }
+
+
+            try{
+
+                int fnameIndex = c.getColumnIndex("firstName");
+                int lastNameIndex = c.getColumnIndex("lastName");
+                int phone = c.getColumnIndex("phoneNumber");
+                int address = c.getColumnIndex("address");
+                int idIndex = c.getColumnIndex("id");
+
+                c.moveToFirst();
+
+                while (c!=null && c.getCount()>0 ){
+                    Log.d("ID :",String.valueOf(c.getInt(idIndex)));
+                    Log.d("Name :",c.getString(fnameIndex));
+                    Log.d("Pos :",String.valueOf(position));
+
+
+                    if(position==c.getInt(idIndex)-1) {
+                        Log.d("ID :",String.valueOf(c.getInt(idIndex)));
+                        Log.d("Name :",c.getString(fnameIndex));
+                        Log.d("Pos :",String.valueOf(position));
+                        holder.firstname.setText(c.getString(fnameIndex));
+                        holder.lastname.setText(c.getString(lastNameIndex));
+                        holder.address.setText(c.getString(address));
+                        holder.phoneNo.setText(c.getString(phone));
+                    }
+
+                    c.moveToNext();
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+            return convertView;
+
+        }
+
+    }
+
+    class ContactHolder{
+        TextView firstname,lastname,phoneNo,address;
+    }
+}
---
+++ app/src/main/java/com/example/assignment_final/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.assignment_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    String first,last,Number,address11;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText etName = (EditText)findViewById(R.id.editText);
+        final EditText etlastName = (EditText)findViewById(R.id.editText1);
+        final EditText etNumber = (EditText)findViewById(R.id.editText3);
+        final EditText etAddress = (EditText)findViewById(R.id.editText5);
+        Button savebutton = (Button)findViewById(R.id.saveButton);
+      //  Button contact_List = (Button)findViewById(R.id.list);
+
+
+
+//        contact_List.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent k = new Intent(MainActivity.this, ContactInfo.class);
+//                startActivity(k);
+//            }
+//        });
+
+
+        savebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                first = etName.getText().toString();
+                last = etlastName.getText().toString();
+                Number = etNumber.getText().toString();
+                address11 = etAddress.getText().toString();
+
+
+                Log.i("first name", first);
+                if(first.isEmpty()  || last.isEmpty()  || Number.isEmpty()  || address11.isEmpty()){
+                    Log.i("heloo", "heloo");
+                }
+                else{
+                    try {
+                        SQLiteDatabase myDatabase = openOrCreateDatabase("person3", MODE_PRIVATE, null);
+
+                        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS person3 (firstName VARCHAR,lastName VARCHAR,phoneNumber VARCHAR,address VARCHAR , id INTEGER PRIMARY KEY)");
+                        myDatabase.execSQL("INSERT INTO person3 (firstName,lastName ,phoneNumber ,address) VALUES ('" +first+ "','" +last+ "', '" +Number+ "', '" +address11+ "')");
+
+                        Cursor c = myDatabase.rawQuery("SELECT * FROM person3", null);
+//
+                        int nameIndex = c.getColumnIndex("firstName");
+                        int lastIndex = c.getColumnIndex("lastName");
+                        int phone = c.getColumnIndex("phoneNumber");
+                        int address = c.getColumnIndex("address");
+                        int idIndex = c.getColumnIndex("id");
+
+                        c.moveToFirst();
+
+                        Intent k = new Intent(MainActivity.this, ContactInfo.class);
+                        startActivity(k);
+
+                        while (c != null) {
+                            Log.i("name", c.getString(nameIndex));
+                            Log.i("name", c.getString(lastIndex));
+                            Log.i("name", c.getString(phone));
+                            Log.i("name", c.getString(address));
+
+                            Log.i("id", c.getString(idIndex));
+
+                            c.moveToNext();
+
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }}
+        });
+
+
+
+    }
+
+
+
+
+}
